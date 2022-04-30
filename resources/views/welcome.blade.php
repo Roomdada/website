5fc84738382326db21847ce40972da39e29a256f
Roomdada <dsieroger@gmail.com>
2022-04-30 16:37:27 +0000

    testimonies section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
                     <h1 class='text-xl text-white font-bold'>Hello, je suis Da Sié Roger.</h1>
                     <h2 class='text-2xl text-green-900 font-bold mb-2'>Développeur & Formateur</h2>
                     <p class='text-sm md:text-base text-white'>
-                        Je suis un jeune dynamique, passionné par le developpement d'application web & mobile ainsi que 
+                        Je suis un jeune dynamique, passionné par le developpement d'application web & mobile ainsi que
                         de nouvelles technologies.
                     </p>
                     <div class='mt-10'>
@@ -182,11 +182,6 @@
                             </div>
                         </div>
                         <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                            <div class="flex-shrink-0">
-                                <img class="h-48 w-full object-cover"
-                                    src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-                                    alt="">
-                            </div>
                             <div class="flex-1 bg-white p-6 flex flex-col justify-between">
                                 <div class="flex-1">
                                     <p class="text-sm font-medium text-indigo-600">
@@ -201,6 +196,12 @@
                                 </div>
 
                             </div>
+                            <div class="flex-shrink-0">
+                                <img class="h-48 w-full object-cover"
+                                    src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+                                    alt="">
+                            </div>
+
                         </div>
                         <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
                             <div class="flex-shrink-0">
@@ -223,12 +224,7 @@
 
                             </div>
                         </div>
-                         <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-                            <div class="flex-shrink-0">
-                                <img class="h-48 w-full object-cover"
-                                    src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-                                    alt="">
-                            </div>
+                        <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
                             <div class="flex-1 bg-white p-6 flex flex-col justify-between">
                                 <div class="flex-1">
                                     <p class="text-sm font-medium text-indigo-600">
@@ -243,6 +239,12 @@
                                 </div>
 
                             </div>
+                            <div class="flex-shrink-0">
+                                <img class="h-48 w-full object-cover"
+                                    src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+                                    alt="">
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -250,79 +252,54 @@
         </div>
     </div>
 
-    <div class='container mx-auto'>
-        <div class="text-center">
-            <h2  class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl mb-6">Ce que disent mes
+    <section class='bg-green-50 py-10'>
+        <div class='container mx-auto'>
+            <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl mb-6 text-center">Ce que disent
+                mes
                 collaborateurs
             </h2>
-            <!-- This example requires Tailwind CSS v2.0+ -->
-            <section id='testimony' class="bg-green-50">
-                <div class="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
-                    <div
-                        class="py-8 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-indigo-900 lg:pr-16">
-                        <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
-                            <div class="relative text-lg font-medium text-white md:flex-grow">
-                                <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-green-500"
-                                    fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
-                                    <path
-                                        d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                                </svg>
-                                <p class="relative text-green-900 font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-                                    expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed
-                                    rerum et corporis.</p>
-                            </div>
-                            <footer class="mt-8">
-                                <div class="flex items-start">
-                                    <div class="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-                                        <img class="h-12 w-12 rounded-full"
-                                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                            alt="">
-                                    </div>
-                                    <div class="ml-4">
-                                        <div class="text-base font-medium">Judith Black</div>
-                                        <div class="text-base font-medium">CEO, Tuple</div>
-                                    </div>
-                                </div>
-                            </footer>
-                        </blockquote>
-                    </div>
-                    <div
-                        class="py-12 px-4 border-t-2 border-indigo-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
-                        <div class="md:flex-shrink-0">
-                            <img class="h-12"
-                                src="https://tailwindui.com/img/logos/workcation-logo-indigo-300.svg" alt="Workcation">
-                        </div>
-                        <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
-                            <div class="relative text-lg font-medium text-white md:flex-grow">
-                                <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-indigo-600"
-                                    fill="currentColor" viewBox="0 0 32 32">
-                                    <path
-                                        d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                                </svg>
-                                <p class="relative">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-                                    expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed
-                                    rerum et corporis. Nemo expedita voluptas culpa sapiente alias molestiae.</p>
-                            </div>
-                            <footer class="mt-8">
-                                <div class="flex items-start">
-                                    <div class="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-                                        <img class="h-12 w-12 rounded-full"
-                                            src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                            alt="">
-                                    </div>
-                                    <div class="ml-4">
-                                        <div class="text-base font-medium text-white">Joseph Rodriguez</div>
-                                        <div class="text-base font-medium text-indigo-200">CEO, Workcation</div>
-                                    </div>
-                                </div>
-                            </footer>
-                        </blockquote>
+            <div class='flex mx-4'>
+                <div class='w-1/3 px-4'>
+                    <div class='bg-green-500 py-24 rounded-xl flex flex-col items-center'>
+                        <img class='rounded-full w-24 h-24 mb-4' src='{{ asset('images/avatar.png') }}'>
+                        <span class='font-bold text-xl'>John Doe</span>
+                        <span class='font-semibold'>Collaborateur</span>
+                        <p class='text-center px-10 mt-4 leading-8 text-white font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Rerum ea amet minima debitis ipsam?
+                            Blanditiis quam delectus voluptates tenetur voluptatum iure accusamus incidunt voluptatibus
+                            facilis vero distinctio, ipsam mollitia provident.
+                        </p>
                     </div>
                 </div>
-            </section>
-        </div>
-    </div>
+                <div class='w-1/3 px-4'>
+                    <div class='bg-green-500 py-24 rounded-xl flex flex-col items-center'>
+                        <img class='rounded-full w-24 h-24 mb-4' src='{{ asset('images/avatar.png') }}'>
+                        <span class='font-bold text-xl'>John Doe</span>
+                        <span class='font-semibold'>Collaborateur</span>
+                        <p class='text-center px-10 mt-4 leading-8 text-white font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Rerum ea amet minima debitis ipsam?
+                            Blanditiis quam delectus voluptates tenetur voluptatum iure accusamus incidunt voluptatibus
+                            facilis vero distinctio, ipsam mollitia provident.
+                        </p>
+                    </div>
+                </div>
+                <div class='w-1/3 px-4'>
+                    <div class='bg-green-500 py-24 rounded-xl flex flex-col items-center'>
+                        <img class='rounded-full w-24 h-24 mb-4' src='{{ asset('images/avatar.png') }}'>
+                        <span class='font-bold text-xl'>John Doe</span>
+                        <span class='font-semibold'>Collaborateur</span>
+                        <p class='text-center px-10 mt-4 leading-8 text-white font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Rerum ea amet minima debitis ipsam?
+                            Blanditiis quam delectus voluptates tenetur voluptatum iure accusamus incidunt voluptatibus
+                            facilis vero distinctio, ipsam mollitia provident.
+                        </p>
+                    </div>
+                </div>
 
-@include('partials.footer')
+            </div>
+        </div>
+    </section>
+    @include('partials.footer')
 </body>
+
 </html>
